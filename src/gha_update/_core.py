05afbc3adc58892680f3d0bb53829bc9d48e80a5
Andrew Esterson <a.esterson@ucl.ac.uk>
2024-12-03 12:16:27 +0000

    ruff linting, cleaned exception messages

--- src/gha_update/_core.py
+++ src/gha_update/_core.py
@@ -89,23 +89,31 @@ async def make_request(client: AsyncClient, url: str) -> Response:
     while True:
         response = await client.get(url)
         if response.status_code == 403:
-            raise Exception(f"Github API rate limit reached, try setting the GITHUB_TOKEN environment variable \n {response.json()}")
+            error_msg = (
+                "Github API rate limit reached, ",
+                "Authenticate by setting the GITHUB_TOKEN environment variable, ",
+                f"{response.json()}",
+            )
+            raise Exception(error_msg)
         elif response.status_code in {301, 302, 307}:
             location = response.headers.get("location")
             if not location:
-                raise Exception(f"Redirected response missing Location header for {url}")
+                error_msg = f"Redirected response missing Location header for {url}"
+                raise Exception(error_msg)
             url = location
         else:
             return response
 
 
 async def get_versions(names: Iterable[str]) -> dict[str, tuple[str, str]]:
-
     tasks: dict[str, Task[Response]] = {}
     headers: dict[str, str] = {}
     if github_token := os.environ.get("GITHUB_TOKEN"):
         headers["Authorization"] = github_token
-    async with AsyncClient(base_url="https://api.github.com", headers=headers) as c, TaskGroup() as tg:
+    async with (
+        AsyncClient(base_url="https://api.github.com", headers=headers) as c,
+        TaskGroup() as tg,
+    ):
         for name in names:
             tasks[name] = tg.create_task(make_request(c, f"/repos/{name}/tags"))
 
